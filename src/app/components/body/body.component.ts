fa1011fa86f65c2ab02cda931484a0d96ed05cec
NormanA1una <baciloferoz@gmail.com>
2023-05-10 15:52:54 -0600

    feat: hola mundo ejercicio

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css']
+  styleUrls: ['./body.component.css'],
 })
 export class BodyComponent {
+  mostrar = true;
 
+  frase: any = {
+    mensaje: 'Un gran poder requiere de una gran responsabilidad.',
+    autor: 'Ben Parker.',
+  };
+
+  products: string[] = [
+    'Toña',
+    'Victoria Clásica',
+    'Victoria Frost',
+    'Miller Light',
+    'Otros',
+  ];
 }
